e73df0c7d37ba9e5174eb086cd04752faecd7b7e
Mukelani Mashoba <131559676+MukelaniGumaMashoba@users.noreply.github.com>
2024-05-23 01:53:54 +0200

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html", "./**/*.js, ./**/*.php", "./*.php", "./*.js", "./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
